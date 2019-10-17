aee8f54659d83203d7e4c5d7291bc8cc260e3f4c
Anh Tran <chirstmax@gmail.com>
2019-10-16 20:52:00 -0400

    remote close date

--- MISService/MISService/Methods/ProjectMethods.cs
+++ MISService/MISService/Methods/ProjectMethods.cs
@@ -48,7 +48,7 @@ namespace MISService.Method
                                            + " (SELECT Id, Stick_Position_Radius__c, Dept_Of_Holes__c, Issue_Date__c, Due_Date__c, Remarks__c FROM StakeOut_Permits__r),"
                                            + " (SELECT Id, Name, First_Site_Contact__c, Second_Site_Contact__c, Budget__c, Provided_By__c,  Remarks__c, Due_Date__c, Rush__c, Requirement__c, Requirement_As_Other__c, Estimated_Shipping_Cost__c, Shipping_Items_Total_Value__c, Work_Order_Number__c  FROM SubContracts__r) "
                                            + " FROM Opportunity "
-                                           + " WHERE Sync__c = true and CloseDate >= TODAY and Middle_Updated_Flag__c = 1 ";
+                                           + " WHERE Sync__c = true and Middle_Updated_Flag__c = 1 ";
                    //                        + " WHERE Middle_Updated_Flag__c = 1 and (Name='(TEST) 20191003' or Name='(TEST) PROJECT AUG 8 2019')  ";
 
 
