0476e31f46318e4077e261d61765dba9fc495c43
Anh Tran <chirstmax@gmail.com>
2019-11-04 20:02:16 -0500

    update code

--- MISService/MISService/Methods/ProjectMethods.cs
+++ MISService/MISService/Methods/ProjectMethods.cs
@@ -48,8 +48,8 @@ namespace MISService.Method
                                            + " (SELECT Id, Stick_Position_Radius__c, Dept_Of_Holes__c, Issue_Date__c, Due_Date__c, Remarks__c FROM StakeOut_Permits__r),"
                                            + " (SELECT Id, Name, First_Site_Contact__c, Second_Site_Contact__c, Budget__c, Provided_By__c,  Remarks__c, Due_Date__c, Rush__c, Requirement__c, Requirement_As_Other__c, Estimated_Shipping_Cost__c, Shipping_Items_Total_Value__c, Work_Order_Number__c  FROM SubContracts__r) "
                                            + " FROM Opportunity "
-                                           + " WHERE Sync__c = true and Middle_Updated_Flag__c = 1 ";
-                   //                        + " WHERE name='(TEST) PROJECT AUG 8 2019'  ";
+                            //              + " WHERE Sync__c = true and Middle_Updated_Flag__c = 1 ";
+                                          + " WHERE name='F.S-Structure-Hugo Boss West Edmonton Mall-Hummingbird'  ";
 
 
                     enterprise.QueryResult result;
